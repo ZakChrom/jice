73f73502665c8d5f1c9ad8875f7af25583b00377
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2026-03-08 19:40:54 +0200

    funny2

--- mc.lua
+++ mc.lua
@@ -86,6 +86,7 @@ function Plugin.before_build()
     local deps = Jice.get_dependencies()
     count = 0
     do_map(deps, {}, count_deps(deps))
+    assert(os.execute("mkdir -p ./.jice/output"));
     if config.resources ~= nil and config.resources ~= "" then
         assert(os.execute("cp -r " .. config.resources .. "/* " .. "./.jice/output/"))
     end
